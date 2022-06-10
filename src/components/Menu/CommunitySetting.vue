6f613b76b44817b1518f05b8c96019ed386d2a27
zjm <zjm97@outlook.com>
2022-06-10 20:52:55 +0800

    9

---
+++ src/components/Menu/CommunitySetting.vue
@@ -0,0 +1,21 @@
+<template>
+    <n-modal v-model:show="show">
+        <n-card>
+            Current community level: 
+            <n-input-number
+                v-model:value="view.communityLevel"
+                @update:value="emits('update:level', $event)"
+            />
+        </n-card>
+    </n-modal>
+</template>
+
+<script setup>
+import { NModal, NCard, NInputNumber } from 'naive-ui';
+import { viewStore } from '../../store/view';
+import { useVModel } from '@vueuse/core'
+const view = viewStore();
+const props = defineProps(["show", "update:level"])
+const emits = defineEmits(["update:show"]);
+const show = useVModel(props, "show", emits);
+</script>
--- src/components/Panel/Panel.vue
+++ src/components/Panel/Panel.vue
@@ -2,19 +2,24 @@
     <div class="flex flex-col">
         <legend-vue />
         <div class="border m-2" h="100px">
-            <histogram-vue 
-                :x="Array.from(stats.count.byDegree.values()).map(v=>v.x0)??[]"
-                :y="histData" 
-                @brush="handleBrush"
-            />
+            <histogram-vue :x="Array.from(stats.count.byDegree.values()).map(v => v.x0) ?? []" :y="histData"
+                @brush="handleBrush" />
         </div>
+        <n-statistic label="Nodes" class="m-2">
+            <n-number-animation :from="0" :to="node_count" />
+        </n-statistic>
+        <n-statistic label="Links" class="m-2">
+            <n-number-animation :from="0" :to="link_count" />
+        </n-statistic>
     </div>
 </template>
 
 <script setup>
+import { NNumberAnimation, NStatistic } from "naive-ui"
 import LegendVue from './Legend.vue';
+import { useCounter } from '@vueuse/core'
 import HistogramVue from './Charts/Histogram.vue';
-import { computed, ref } from 'vue';
+import { computed, ref, watchEffect } from 'vue';
 import { node_stat } from "../../algorithms/statistics";
 import _ from "lodash"
 
@@ -27,14 +32,28 @@ const stats = computed(() => {
 const histData = computed(() => {
     return _(stats.value.count.byDegree).toPairs().sortBy(i => i[0]).map(i => i[1].length).value()
 });
-function handleBrush(e){
+function handleBrush(e) {
     const [x0, x1] = e;
-    const nodes = new Map(_(stats.value.degree).toPairs().filter(([k, v])=>{
-        return v>=x0&&v<=x1;
-    }).map(([k,v])=>[k, props.graph.getNode(k)]).value());
+    const nodes = new Map(_(stats.value.degree).toPairs().filter(([k, v]) => {
+        return v >= x0 && v <= x1;
+    }).map(([k, v]) => [k, props.graph.getNode(k)]).value());
     // console.log(nodes,_(stats.value.degree).toPairs().filter(([k, v])=>{
     //     return v>=x0&&v<=x1;
     // }).map(([k,v])=>[k, graph.getNode(k)]).value())
     emits("select", nodes)
 }
+const node_count = computed(() => {
+    let c = 0;
+    props.graph.forEachNode(() => {
+        c++;
+    });
+    return c
+})
+const link_count = computed(() => {
+    let c = 0;
+    props.graph.forEachLink(() => {
+        c++;
+    });
+    return c
+})
 </script>
